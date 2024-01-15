ebbda84c3f175f1ee44d19950c480a1a4d7979e3
Little_Q <1179177709@qq.com>
2024-01-15 16:44:24 +0800

    初始化

---
+++ src/views/HomeView.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { onMounted } from 'vue'
+import * as Cesium from 'cesium'
+//cesium初始化必须写在mounted生命周期里面，否则会报错"Element with id "cesiumContainer" does not exist in the document."
+onMounted(() => {
+  new Cesium.Viewer('cesium-contanier', {
+    //这里是配置项
+  })
+})
+
+</script>
+
+<template>
+  <div class="cesium-contanier" id="cesium-contanier"></div>
+</template>
+
+<style>
+.cesium-contanier {
+  width: 100%;
+  height: 100%;
+}
+</style>
